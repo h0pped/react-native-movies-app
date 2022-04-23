d07b7f2e85fbfa60cda6ae12502a9bf9504eefb8
h0pped <notawril@gmail.com>
2022-04-23 11:32:45 +0200

    tab navigation

--- App.js
+++ App.js
@@ -4,32 +4,79 @@ import Home from './screens/Home';
 import Detail from './screens/Detail';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Search from './screens/Search';
+import Profile from './screens/Profile';
 // import {getPopularMovies} from './services/services';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = createNativeStackNavigator();
+const SearchStack = createNativeStackNavigator();
+const ProfileStack = createNativeStackNavigator();
+
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="HomeStack"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen name="Detail" component={Detail} />
+    </HomeStack.Navigator>
+  );
+};
+const SearchStackScreen = () => {
+  return (
+    <SearchStack.Navigator>
+      <SearchStack.Screen name="SearchStack" component={Search} />
+    </SearchStack.Navigator>
+  );
+};
+const ProfileStackScreen = () => {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen name="ProfileStack" component={Profile} />
+    </ProfileStack.Navigator>
+  );
+};
 const App = () => {
-  // const arr = ['hello', 'there', 'Illia'];
-  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="Detail" component={Detail} />
-        {/* <View style={styles.container}> */}
-        {/* <Home /> */}
-        {/* </View> */}
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-home' : 'ios-home-outline';
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'ios-search' : 'ios-search';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          headerShown: false,
+          activeTintColor: 'blue',
+          inactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Search" component={SearchStackScreen} />
+        <Tab.Screen name="Profile" component={ProfileStackScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
 export default App;
---
+++ components/BottomNavigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
+  );
+}
+
+export default MyTabs;
